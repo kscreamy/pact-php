b9ec9e07fb16825bed702455cb0f0f5716116097
placebo <placebo@syneforge.com>
2017-11-06 17:05:47 +0200

    remove httplug integration

--- src/Mocks/MockHttpService/MockProviderService.php
+++ src/Mocks/MockHttpService/MockProviderService.php
@@ -26,11 +26,6 @@ class MockProviderService implements IMockProviderService
     private $_host;
 
     /**
-     * @var MockHttpClient
-     */
-    private $_client;
-
-    /**
      * @var \Windwalker\Http\HttpClient
      */
     private $_httpClient;
@@ -60,7 +55,6 @@ class MockProviderService implements IMockProviderService
         $this->_config = $config;
         $this->_httpClient = new \Windwalker\Http\HttpClient();
         $this->_host = new MockProviderHost();
-        $this->_client = new MockHttpClient($this->_host);
 
         $pactFile = new ProviderServicePactFile();
         $this->setPactFile($pactFile);
@@ -102,14 +96,6 @@ class MockProviderService implements IMockProviderService
     public function getHost()
     {
         return $this->_host;
-    }
-
-    /**
-     * @return MockHttpClient
-     */
-    public function getClient()
-    {
-        return $this->_client;
     }
 
     /**
